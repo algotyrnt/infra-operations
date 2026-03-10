7555ec2fd27dd70f3ce48a8700ea004685b2f412
Tharindu Thenuwara <tharindut@wso2.com>
2026-03-10 18:58:07 +0530

    Remove error display settings based on APP_ENV
    Removed error display settings for production environment.

--- apps/file-integrity-monitor/dashboard/data-analyzer/app/view_full_data.php
+++ apps/file-integrity-monitor/dashboard/data-analyzer/app/view_full_data.php
@@ -16,14 +16,6 @@
  * under the License. 
  */
 
-if (getenv('APP_ENV') === 'development') {
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-} else {
-    ini_set('display_errors', '0');
-    ini_set('display_startup_errors', '0');
-}
 
 session_start();
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
